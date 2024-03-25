1b6208099069272461c45ae67b827b8c5060cdcc
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2024-03-25 18:01:36 +0200

    uploaded after tree traversal

--- tree.js
+++ tree.js
@@ -34,6 +34,7 @@ class BinarySearchTree {
       }
     }
   }
+
   find(value) {
     if (this.root === null) return false;
     var current = this.root,
@@ -50,6 +51,7 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
   contains(value) {
     if (this.root === null) return false;
     var current = this.root,
@@ -65,17 +67,66 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  BFS() {
+    var node = this.root,
+      queue = [],
+      visited = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      visited.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return visited;
+  }
+
+  DFSPreOrder() {
+    var visitedRes = [];
+    function traverse(node) {
+      visitedRes.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visitedRes;
+  }
+  DFSPostOrder() {
+    var visitedRes = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visitedRes.push(node.value);
+    }
+    traverse(this.root);
+    return visitedRes;
+  }
+  DFSInOrder() {
+    var visitedRes = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visitedRes.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visitedRes;
+  }
 }
 
 //      10
-//   5     13
-// 2  7  11  16
+//   6     15
+// 3  8    20
 
 var tree = new BinarySearchTree();
+var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInOrder();
